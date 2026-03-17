0eb60ac37e00a382505ac018a2fa25e02cde3131
Esa <esa-pitkanen@hotmail.com>
2026-03-17 10:16:07 +0200

    feat: add initial routes to the pages

---
+++ src/routes.jsx
@@ -0,0 +1,20 @@
+import App from "./App.jsx";
+import Shop from "./pages/Shop.jsx";
+import Cart from "./pages/Cart.jsx";
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/shop",
+    element: <Shop />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+];
+
+export default routes
