9595e98f9f7c5e12a1465ddbbb494bd6f1bec9bf
Esa <esa-pitkanen@hotmail.com>
2026-03-17 11:13:28 +0200

    feat: add Header component
    - add Navigation component to header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Navigation from "./Navigation.jsx";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header-content">
+        <Navigation />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
