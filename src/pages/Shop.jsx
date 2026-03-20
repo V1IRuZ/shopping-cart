3ecbab312aaa59ca0b413c1a9fe808b2bef27df7
Esa <esa-pitkanen@hotmail.com>
2026-03-20 12:53:15 +0200

    feat: fetch data from FakeStore API and display it to ProductCards

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,24 @@
+import { useState, useEffect } from "react";
 import PageLayout from "../components/PageLayout.jsx";
+import ProductCard from "../components/ProductCard.jsx";
 
 const Shop = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, []);
+
   return (
     <PageLayout>
       <h1>Shop</h1>
+      <div className="products">
+        {data.map((product) => (
+          <ProductCard product={product} />
+        ))}
+      </div>
     </PageLayout>
   );
 };
