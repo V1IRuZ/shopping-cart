7ae02eec7ff4eb6043e3c07320676ce02bba7fc8
Esa <esa-pitkanen@hotmail.com>
2026-03-21 15:11:45 +0200

    feat: pass data, loading and error to context
    - retrieve data, loading and error from useProductsData hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import PageLayout from "./components/PageLayout.jsx";
 import { Outlet } from "react-router";
+import { useProductData } from "./hooks/useProductsData.js";
 
 function App() {
+  const { data, loading, error } = useProductData();
+
   return (
     <PageLayout>
-      <Outlet />
+      <Outlet context={{ data, loading, error }} />
     </PageLayout>
   );
 }
