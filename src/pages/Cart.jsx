907d714c95585b992798f3c7f8d4b936ec242342
Esa <esa-pitkanen@hotmail.com>
2026-03-22 18:24:19 +0200

    feat: apply CartList to Cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,13 @@
+import { useOutletContext } from "react-router";
+import CartList from "../components/CartList.jsx";
+
 const Cart = () => {
+  const { cart, setCart } = useOutletContext();
+
   return (
     <>
       <h1>Cart</h1>
+      <CartList cart={cart} />
     </>
   );
 };
