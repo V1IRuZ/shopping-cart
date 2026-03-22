01eb987ff1c1e862b70d3e6247567164fb588688
Esa <esa-pitkanen@hotmail.com>
2026-03-22 18:16:27 +0200

    feat: add new state for cart items

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import PageLayout from "./components/PageLayout.jsx";
 import { Outlet } from "react-router";
@@ -5,10 +6,11 @@ import { useProductData } from "./hooks/useProductsData.js";
 
 function App() {
   const { data, loading, error } = useProductData();
+  const [cart, setCart] = useState([]);
 
   return (
     <PageLayout>
-      <Outlet context={{ data, loading, error }} />
+      <Outlet context={{ data, loading, error, cart, setCart }} />
     </PageLayout>
   );
 }
