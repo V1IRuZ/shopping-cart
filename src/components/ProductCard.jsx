9dfe8ab8f2d566154f726460f9027c582c2d0850
Esa <esa-pitkanen@hotmail.com>
2026-03-22 13:14:33 +0200

    feat: wrap product info elements inside Link

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router";
+
 const ProductCard = ({ product }) => {
   return (
     <div className="product-card">
       <img src={product.image} alt="" />
       <div className="product-info">
-        <h1 className="product-title">{product.title}</h1>
-        <h1 className="product-price">{product.price} $</h1>
+        <Link to={`/shop/product/${product.id}`}>
+          <h1 className="product-title">{product.title}</h1>
+          <h1 className="product-price">{product.price} $</h1>
+        </Link>
       </div>
     </div>
   );
