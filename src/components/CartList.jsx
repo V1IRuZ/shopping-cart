578380d6811abb4520ecb60d07643fc08aa334fa
Esa <esa-pitkanen@hotmail.com>
2026-03-23 19:11:41 +0200

    feat: add product removal handler

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -1,14 +1,24 @@
 import CartItem from "./CartItem.jsx";
 
-const CartList = ({ cart }) => {
+const CartList = ({ cart, setCart }) => {
   if (cart.length <= 0) {
     return <h1>Cart is empty...</h1>;
   }
 
+  const handleRemove = (id) => {
+    const updatedCart = cart.filter((item) => item.id !== id);
+
+    setCart(updatedCart);
+  };
+
   return (
     <ul>
       {cart.map((item) => (
-        <CartItem key={item.id} product={item} />
+        <CartItem
+          key={item.id}
+          product={item}
+          onRemove={() => handleRemove(item.id)}
+        />
       ))}
     </ul>
   );
