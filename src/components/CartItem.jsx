7aa06d02efc1d32872b66badc67c83805ab0cb49
Esa <esa-pitkanen@hotmail.com>
2026-03-23 19:06:32 +0200

    feat: edit product view on Cart page
    - The link in the product title takes you to the product page
    - added a button to remove the product from the shopping cart
    - added product image

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,11 +1,24 @@
-const CartItem = ({ product }) => {
+import { Link } from "react-router";
+import { Trash } from "lucide-react";
+
+const CartItem = ({ product, onRemove }) => {
   const totalPrice = product.quantity * product.price;
 
   return (
     <li className="cart-item">
-      <h1>{product.title}</h1>
-      <h1>Quantity: {product.quantity}</h1>
-      <h1>Price: {totalPrice}</h1>
+      <Link to={`/shop/product/${product.id}`}>
+        <img src={product.image} alt="" />
+        <h1 className="title">{product.title}</h1>
+      </Link>
+      <div className="item-total">
+        <div className="product-options">
+          <h2 className="quantity">Quantity: {product.quantity}</h2>
+          <button onClick={onRemove}>
+            <Trash size={24} />
+          </button>
+        </div>
+        <h2 className="price">Price: {totalPrice} $</h2>
+      </div>
     </li>
   );
 };
