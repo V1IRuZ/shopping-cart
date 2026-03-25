86a68675fca6bd269cdcbee06d0835b6b81b01bd
Esa <esa-pitkanen@hotmail.com>
2026-03-25 17:09:34 +0200

    feat: implement handleChange function
    - function accepts orders with a maximum of two-digit quantities

--- src/components/CartList.jsx
+++ src/components/CartList.jsx
@@ -5,6 +5,21 @@ const CartList = ({ cart, setCart }) => {
     return <h1>Cart is empty...</h1>;
   }
 
+  const handleChange = (e, id) => {
+    const inputValue = Number(e.target.value);
+    const regex = /^\d{0,2}$/;
+
+    if (!regex.test(inputValue)) {
+      return;
+    }
+
+    setCart((prev) =>
+      prev.map((item) =>
+        item.id === id ? { ...item, quantity: inputValue } : item,
+      ),
+    );
+  };
+
   const handleRemove = (id) => {
     const updatedCart = cart.filter((item) => item.id !== id);
 
@@ -18,6 +33,7 @@ const CartList = ({ cart, setCart }) => {
           key={item.id}
           product={item}
           onRemove={() => handleRemove(item.id)}
+          handleChange={handleChange}
         />
       ))}
     </ul>
