699b78bb5b58d13fdb72f688f7ada505ca665b92
Esa <esa-pitkanen@hotmail.com>
2026-03-25 18:14:58 +0200

    fix: prevent input field numbers from starting with zero
    - change input type to text
    - remove maxLength attribute

--- src/components/ProductCounter.jsx
+++ src/components/ProductCounter.jsx
@@ -1,15 +1,19 @@
 const ProductCounter = ({ product, setCart, handleRemove }) => {
   const handleChange = (e, id) => {
-    const inputValue = Number(e.target.value);
+    const inputValue = e.target.value;
     const regex = /^\d{0,2}$/;
 
     if (!regex.test(inputValue)) {
       return;
     }
 
+    if (inputValue === "0") {
+      return;
+    }
+
     setCart((prev) =>
       prev.map((item) =>
-        item.id === id ? { ...item, quantity: inputValue } : item,
+        item.id === id ? { ...item, quantity: Number(inputValue) } : item,
       ),
     );
   };
@@ -26,8 +30,7 @@ const ProductCounter = ({ product, setCart, handleRemove }) => {
       <button>-</button>
       <label htmlFor="">
         <input
-          type="number"
-          maxLength={2}
+          type="text"
           onChange={(e) => handleChange(e, product.id)}
           value={product.quantity}
           onBlur={(e) => handleBlur(e)}
