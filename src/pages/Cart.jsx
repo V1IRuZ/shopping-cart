72db2aff4982353fa3aed18c435452ff036c3831
Esa <esa-pitkanen@hotmail.com>
2026-03-26 17:12:59 +0200

    feat: add Checkout component to Cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,17 @@
 import { useOutletContext } from "react-router";
 import CartList from "../components/CartList.jsx";
+import Checkout from "../components/Checkout.jsx";
 
 const Cart = () => {
   const { cart, setCart } = useOutletContext();
 
-   return (
+  const totalItems = cart.reduce((total, item) => (total += item.quantity), 0);
+
+  return (
     <>
-      <h1>Cart</h1>
+      <h1>There are {totalItems} products in your cart</h1>
       <CartList cart={cart} setCart={setCart} />
+      <Checkout cart={cart} />
     </>
   );
 };
