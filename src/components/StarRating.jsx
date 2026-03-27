913dbe3b84d421a8b3cae9b329fa83e740638437
Esa <esa-pitkanen@hotmail.com>
2026-03-27 19:47:43 +0200

    feat: add StarRating component

---
+++ src/components/StarRating.jsx
@@ -0,0 +1,22 @@
+import { Star } from "lucide-react";
+
+const StarRating = ({ rating }) => {
+  const integer = Math.floor(rating);
+  const array = [0, 1, 2, 3, 4];
+
+  return (
+    <div className="star-rating" style={{display: "flex"}}>
+      {array.map((value) =>
+        value < rating && value < integer ? (
+          <Star fill="gold" />
+        ) : value < rating && value === integer ? (
+          <Star fill="#fdec8a" />
+        ) : (
+          <Star fill="white" />
+        ),
+      )}
+    </div>
+  );
+};
+
+export default StarRating;
