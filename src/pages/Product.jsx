0867b53a7f2da8d1d3d900d254c80674e50a2c01
Esa <esa-pitkanen@hotmail.com>
2026-03-28 20:17:14 +0200

    feat: define product information that is displayed on the page
    - add StarRating component
    - add additional wrapper elements for layout

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,14 +1,28 @@
+import Button from "../components/ui/Button.jsx";
+import StarRating from "../components/StarRating.jsx";
+
 const Product = ({ product }) => {
   return (
     <div className="product">
-      <div className="title">
-        <h1>{product.title}</h1>
+      <div className="details">
+        <div className="title">
+          <h1>{product.title}</h1>
+          <StarRating
+            rating={product.rating.rate}
+            count={product.rating.count}
+          />
+        </div>
+        <div className="description">
+          <h2>Product details</h2>
+          <p>{product.description}</p>
+        </div>
+        <div className="add">
+          <Button className="add-btn" label="ADD TO CART" />
+        </div>
+      </div>
+      <div className="image">
         <img src={product.image} alt="" />
       </div>
-      <div className="description">
-        <p>{product.description}</p>
-      </div>
-      <button>BUY</button>
     </div>
   );
 };
