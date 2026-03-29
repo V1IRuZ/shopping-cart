b9b4f54a00fa4a66b31eaad9ab9e278425bf6600
Esa <esa-pitkanen@hotmail.com>
2026-03-29 17:41:01 +0300

    feat: add all product details to the card component

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,11 +1,32 @@
+import { Link } from "react-router";
 import StarRating from "./StarRating.jsx";
+import styles from "../styles/CategoryCard.module.css";
+import Button from "./ui/Button.jsx";
 
 const CategoryCard = ({ product }) => {
   return (
-    <li className="category-card">
-      <div>
-        <h1>{product.title}</h1>
-        <StarRating rating={product.rating.rate} count={product.rating.count} />
+    <li className={styles.card}>
+      <Link
+        className={styles.link}
+        to={`/shop/${product.category}/${product.id}`}
+      >
+        <div className={styles["image-wrapper"]}>
+          <img src={product.image} alt="" className={styles.image} />
+        </div>
+        <div className={styles.info}>
+          <h1 className={styles.title}>{product.title}</h1>
+          <StarRating
+            rating={product.rating.rate}
+            count={product.rating.count}
+          />
+          <p className={styles.description}>{product.description}</p>
+        </div>
+      </Link>
+      <div className={styles.order}>
+        <h2 className={styles.price}>{product.price} $</h2>
+        <div>
+          <Button label="ADD TO CART" className="add-btn" />
+        </div>
       </div>
     </li>
   );
