5b43740eaa7cab968da07e11b60664fde0065dac
Esa <esa-pitkanen@hotmail.com>
2026-03-29 15:15:28 +0300

    feat: add Category component

---
+++ src/pages/Category.jsx
@@ -0,0 +1,21 @@
+import CategoryCard from "../components/CategoryCard.jsx";
+import { useParams, useOutletContext } from "react-router";
+
+const Category = () => {
+  const { data } = useOutletContext();
+  const { category } = useParams();
+
+  const formatPath = decodeURIComponent(category);
+
+  const selectedCategory = data.filter((item) => item.category === formatPath);
+
+  return (
+    <ul className="category">
+      {selectedCategory.map((item) => (
+        <CategoryCard key={item.id} product={item} />
+      ))}
+    </ul>
+  );
+};
+
+export default Category;
