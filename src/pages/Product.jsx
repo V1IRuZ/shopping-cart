d0bacf4ac2ff7e8b1ca154e1cbd6fce3819abcac
Esa <esa-pitkanen@hotmail.com>
2026-03-29 19:47:28 +0300

    feat: apply PathNavigation component to product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,29 +1,33 @@
 import Button from "../components/ui/Button.jsx";
 import StarRating from "../components/StarRating.jsx";
+import PathNavigation from "../components/PathNavigation.jsx";
 
 const Product = ({ product }) => {
   return (
-    <div className="product">
-      <div className="details">
-        <div className="title">
-          <h1>{product.title}</h1>
-          <StarRating
-            rating={product.rating.rate}
-            count={product.rating.count}
-          />
+    <>
+      <PathNavigation product={product} />
+      <div className="product">
+        <div className="details">
+          <div className="title">
+            <h1>{product.title}</h1>
+            <StarRating
+              rating={product.rating.rate}
+              count={product.rating.count}
+            />
+          </div>
+          <div className="description">
+            <h2>Product details</h2>
+            <p>{product.description}</p>
+          </div>
+          <div className="add">
+            <Button className="add-btn" label="ADD TO CART" />
+          </div>
         </div>
-        <div className="description">
-          <h2>Product details</h2>
-          <p>{product.description}</p>
-        </div>
-        <div className="add">
-          <Button className="add-btn" label="ADD TO CART" />
+        <div className="image">
+          <img src={product.image} alt="" />
         </div>
       </div>
-      <div className="image">
-        <img src={product.image} alt="" />
-      </div>
-    </div>
+    </>
   );
 };
 
