564157eae86f5b18bd14d9d1fe2389124dbb4967
Esa <esa-pitkanen@hotmail.com>
2026-03-31 11:22:17 +0300

    feat: add icon to error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,6 +1,9 @@
+import { CircleX } from "lucide-react";
+
 const ErrorPage = () => {
   return (
     <div className="error">
+      <CircleX size={36} color="red" />
       <h1>Something went wrong, try again later.</h1>
     </div>
   );
