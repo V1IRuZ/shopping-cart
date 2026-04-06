6edd7de39b7646ee657616f4a041bfd0231d91d7
Esa <esa-pitkanen@hotmail.com>
2026-04-06 13:08:51 +0300

    refactor: extract product page styles to own file
    - import styles to ProductPage component

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,11 +1,12 @@
 import Product from "../features/product/Product.jsx";
 import { useOutletContext, useParams } from "react-router";
+import "../styles/ProductPage.css";
 
 const ProductPage = () => {
   const { data } = useOutletContext();
   let { id } = useParams();
 
-  const currentProduct = data.find((item) => item.id === Number(id))  
+  const currentProduct = data.find((item) => item.id === Number(id));
 
   return <Product product={currentProduct} />;
 };
