04fb3d72caef8bf46cee2fb809ca03878a3475df
Esa <esa-pitkanen@hotmail.com>
2026-04-08 18:50:44 +0300

    feat: implement handlers for switching slide in buttons

--- src/features/slideshow/Carousel.jsx
+++ src/features/slideshow/Carousel.jsx
@@ -1,13 +1,35 @@
 import { useState } from "react";
 import Slides from "./Slides.jsx";
+import NextButton from "./NextButton.jsx";
+import PreviousButton from "./PreviousButton.jsx";
 import "../../styles/Carousel.css";
 
 const Carousel = () => {
   const [activeSlide, setActiveSlide] = useState(0);
 
+  const handleNext = () => {
+    if (activeSlide >= 3) {
+      setActiveSlide(0);
+      return;
+    }
+
+    setActiveSlide((prev) => prev + 1);
+  };
+
+  const handlePrevious = () => {
+    if (activeSlide <= 0) {
+      setActiveSlide(3);
+      return;
+    }
+
+    setActiveSlide((prev) => prev - 1);
+  };
+
   return (
     <div className="carousel">
+      <PreviousButton onClick={handlePrevious} />
       <Slides activeSlide={activeSlide} />
+      <NextButton onClick={handleNext} />
     </div>
   );
 };
