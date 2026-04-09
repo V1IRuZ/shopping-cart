1192df043571d48d9a83891dc2cf52ead474064b
Esa <esa-pitkanen@hotmail.com>
2026-04-09 18:19:25 +0300

    feat: add content to first slide

--- src/features/slideshow/SlideOne.jsx
+++ src/features/slideshow/SlideOne.jsx
@@ -1,12 +1,28 @@
-const SlideOne = ({ active }) => {
+import { Link } from "react-router";
+
+const SlideOne = ({ data, active }) => {
+  const bed = data.find((item) => item.id === 11);
+
   return (
-    <div className="slide first">
-      <h2>Slide 1</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-        voluptatum iste laudantium, delectus ratione et voluptas similique
-        deserunt nihil ab illo quia repudiandae.
-      </p>
+    <div className="slide first" inert={!active} aria-hidden={!active}>
+      <div className="images">
+        <img src={bed.images[0]} alt="" />
+        <img src={bed.images[1]} alt="" />
+        <img src={bed.images[2]} alt="" />
+        <img src={bed.thumbnail} alt="" />
+      </div>
+      <div className="info">
+        <h3>{bed.title}</h3>
+        <p>{bed.description}</p>
+        <div className="link">
+          <Link
+            tabIndex={active ? 0 : -1}
+            to={`/shop/${bed.category}/${bed.id}`}
+          >
+            SEE MORE
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
