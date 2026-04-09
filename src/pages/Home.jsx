b50b6c61b6d2476af5a1fb6f462b97347e52be0c
Esa <esa-pitkanen@hotmail.com>
2026-04-09 18:14:54 +0300

    feat: apply Carousel to home page and pass data as props

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,15 @@
+import Carousel from "../features/slideshow/Carousel.jsx";
+import { useOutletContext } from "react-router";
+
 const Home = () => {
-  return <h1>This is Home page!</h1>;
+  const { data } = useOutletContext();
+
+  return (
+    <>
+      <h1>This is Home page!</h1>
+      <Carousel data={data} />
+    </>
+  );
 };
 
 export default Home;
