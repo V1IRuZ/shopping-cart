96f9535d8622b061344184a34ea38a01938b6311
Esa <esa-pitkanen@hotmail.com>
2026-04-10 18:30:31 +0300

    feat: create content for slide 2

--- src/features/slideshow/SlideTwo.jsx
+++ src/features/slideshow/SlideTwo.jsx
@@ -1,11 +1,27 @@
-const SlideTwo = ({ data, active }) => {
+import { Link } from "react-router";
+
+const FragranceItem = ({ product }) => {
   return (
-    <div className="slide two">
-      <h2>Slide 2</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi,
-        voluptatum nisi! Eos non tempora beatae deserunt?
-      </p>
+    <li className="fragrance-item">
+      <img src={product.thumbnail} alt="" />
+      <h4>{product.brand}</h4>
+    </li>
+  );
+};
+
+const SlideTwo = ({ data, active }) => {
+  const fragrances = data?.filter((item) => item.category === "fragrances");
+
+  return (
+    <div className="slide two" inert={!active} aria-hidden={!active}>
+      <Link tabIndex={active ? 0 : -1} to="/shop/fragrances">
+        <h3>SEE OUR SELECTION OF FRAGRANCES</h3>
+        <ul className="fragrances">
+          {fragrances.map((item) => (
+            <FragranceItem product={item} key={item.id} />
+          ))}
+        </ul>
+      </Link>
     </div>
   );
 };
