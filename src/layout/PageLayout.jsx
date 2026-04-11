18a24872af185abedf16d0e276e6774cdc89d8fb
Esa <esa-pitkanen@hotmail.com>
2026-04-11 18:49:50 +0300

    feat: add main container for page

--- src/layout/PageLayout.jsx
+++ src/layout/PageLayout.jsx
@@ -3,11 +3,11 @@ import Footer from "./Footer.jsx";
 
 const PageLayout = ({ children, cart }) => {
   return (
-    <>
+    <div className="page">
       <Header cart={cart}/>
       <main className="content">{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
