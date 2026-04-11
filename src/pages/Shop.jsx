32b55ea73b119fe3d3748c23866b7ff6a6629f56
Esa <esa-pitkanen@hotmail.com>
2026-04-11 20:18:47 +0300

    refactor: filter each category into its own section
    - used custom hook for filtering data
    - added sageguard if data is not retrieved

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,23 +1,42 @@
-import ProductCard from "../features/shop/ProductCard.jsx";
+import CategorySection from "../features/shop/CategorySection.jsx";
 import CategoryNavigation from "../features/shop/CategoryNavigation.jsx";
+import { useCategories } from "../hooks/useCategories.js";
 import { useOutletContext } from "react-router";
 import "../styles/Shop.css";
 
 const Shop = () => {
   const { data, handleAddToCart } = useOutletContext();
+  const categories = useCategories(data);
+
+  if (!categories) return null;
 
   return (
     <>
       <CategoryNavigation />
-      <div className="products">
-        {data.map((product) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            onAddtoCart={() => handleAddToCart(product)}
-          />
-        ))}
-      </div>
+      <CategorySection
+        category={categories.beauty}
+        handleAddToCart={handleAddToCart}
+        sectionClassName="beauty"
+        headerText="Our beauty products"
+      />
+      <CategorySection
+        category={categories.fragrances}
+        handleAddToCart={handleAddToCart}
+        sectionClassName="fragrances"
+        headerText="Find our fragrances here"
+      />
+      <CategorySection
+        category={categories.furniture}
+        handleAddToCart={handleAddToCart}
+        sectionClassName="furniture"
+        headerText="Our stylish furniture"
+      />
+      <CategorySection
+        category={categories.groceries}
+        handleAddToCart={handleAddToCart}
+        sectionClassName="groceries"
+        headerText="Daily grocery shopping"
+      />
     </>
   );
 };
