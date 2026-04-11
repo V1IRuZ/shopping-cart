1890f5cae8f7b94ec417bf168521b08bd54bbdd3
Esa <esa-pitkanen@hotmail.com>
2026-04-11 18:47:48 +0300

    feat: create content for slide four

--- src/features/slideshow/SlideFour.jsx
+++ src/features/slideshow/SlideFour.jsx
@@ -1,11 +1,20 @@
+import { Link } from "react-router";
+
 const SlideFour = ({ data, active }) => {
+  const items = data?.filter((item) => item.category === "beauty");
+
+  if (!items) return null;
+
   return (
-    <div className="slide four">
-      <h2>Slide 4</h2>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit
-        consectetur nisi quisquam voluptatem amet eaque fugiat?
-      </p>
+    <div className="slide four" inert={!active} aria-hidden={!active}>
+      <Link tabIndex={active ? 0 : -1} to="/shop/beauty">
+        <h3>Beauty products</h3>
+        <div className="items">
+          {items.map((item) => (
+            <img src={item.thumbnail} />
+          ))}
+        </div>
+      </Link>
     </div>
   );
 };
