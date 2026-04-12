da0d01ac199093b8cae2a04b82300930d2c2af78
Esa <esa-pitkanen@hotmail.com>
2026-04-12 18:35:03 +0300

    feat: add Logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+const Logo = () => {
+  return (
+    <div className="logo">
+      <span className="web">Web</span>
+      <span className="market">Market</span>
+    </div>
+  );
+};
+
+export default Logo;
