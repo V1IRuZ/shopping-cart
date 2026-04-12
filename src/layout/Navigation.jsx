f6d67aa78cc5fd885af829b9a0486acf8fc7fbf7
Esa <esa-pitkanen@hotmail.com>
2026-04-12 15:18:14 +0300

    feat: apply icons to Links and wrap text with span elements

--- src/layout/Navigation.jsx
+++ src/layout/Navigation.jsx
@@ -1,22 +1,31 @@
 import { Link } from "react-router";
-import { ShoppingCart } from "lucide-react";
+import { ShoppingCart, Store, House } from "lucide-react";
 
 const Navigation = ({ cart }) => {
   const totalItems = cart.reduce((total, item) => (total += item.quantity), 0);
 
   return (
     <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
+      <ul className="main-links">
+        <li className="home-link">
+          <Link to="/">
+            <House size={36} />
+            <span>HOME</span>
+          </Link>
         </li>
-        <li>
-          <Link to="/shop">Shop</Link>
+        <li className="shop-link">
+          <Link to="/shop">
+            <Store size={36} />
+            <span>SHOP</span>
+          </Link>
         </li>
         <li className="cart-link">
-          <Link to="/cart">
-            {totalItems > 0 && <span>{totalItems}</span>}
-            <ShoppingCart size={48} />
+          <Link to="/cart" className="cart-svg">
+            {totalItems > 0 && (
+              <span className="total-items">{totalItems}</span>
+            )}
+            <ShoppingCart size={36} />
+            <span className="cart-link-title">CART</span>
           </Link>
         </li>
       </ul>
