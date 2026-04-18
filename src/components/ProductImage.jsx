3bceb7ad57a48e03cc257617a0847890d964c541
Esa <esa-pitkanen@hotmail.com>
2026-04-18 19:58:07 +0300

    feat: add ProductImage component
    - if product is on sale, display image with discount

---
+++ src/components/ProductImage.jsx
@@ -0,0 +1,21 @@
+import styles from "../styles/ProductImage.module.css";
+
+const ProductImage = ({ product }) => {
+  if (product.isDiscount) {
+    const roundedDiscount = Math.round(product.discountPercentage);
+    return (
+      <div className={styles.wrapper}>
+        <span className={styles.discount}>-{roundedDiscount}%</span>
+        <img className={styles.image} src={product.thumbnail} alt="" />
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <img src={product.thumbnail} alt="" />
+    </div>
+  );
+};
+
+export default ProductImage;
