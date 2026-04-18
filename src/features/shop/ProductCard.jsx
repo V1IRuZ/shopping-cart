024864eb663435f5ecbf564315a34c69a1e9ad41
Esa <esa-pitkanen@hotmail.com>
2026-04-18 18:34:10 +0300

    refactor: wrap the image inside the link

--- src/features/shop/ProductCard.jsx
+++ src/features/shop/ProductCard.jsx
@@ -6,14 +6,14 @@ import StarRating from "../../components/StarRating.jsx";
 const ProductCard = ({ product, onAddtoCart }) => {
   return (
     <li className="product-card">
-      <img src={product.thumbnail} alt="" />
-      <div className="product-info">
-        <Link to={`/shop/${product.category}/${product.id}`}>
+      <Link to={`/shop/${product.category}/${product.id}`}>
+        <img src={product.thumbnail} alt="" />
+        <div className="product-info">
           <h1 className="product-title">{product.title}</h1>
           <StarRating key={product.id} rating={product.rating} />
           <Price product={product} className="product-price" />
-        </Link>
-      </div>
+        </div>
+      </Link>
       <Button label="ADD TO CART" className="add-btn" onClick={onAddtoCart} />
     </li>
   );
