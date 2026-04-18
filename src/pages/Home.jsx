2c870008672b62248859d56861d026d01e1ccc82
Esa <esa-pitkanen@hotmail.com>
2026-04-18 15:38:08 +0300

    feat: apply groceries banner to the home page
    - remove placeholder text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import Carousel from "../features/slideshow/Carousel.jsx";
+import FreshGroceriesSection from "../features/home/FreshGroceriesSection.jsx";
 import { useOutletContext } from "react-router";
+import "../styles/Home.css";
 
 const Home = () => {
   const { data } = useOutletContext();
 
   return (
     <>
-      <h1>This is Home page!</h1>
       <Carousel data={data} />
+      <FreshGroceriesSection />
     </>
   );
 };
