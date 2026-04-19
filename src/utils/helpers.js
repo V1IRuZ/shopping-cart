f7bad31595b6bb846182163cf70986a42d387a83
Esa <esa-pitkanen@hotmail.com>
2026-04-19 17:38:06 +0300

    feat: add calculateCurrentPrice helper function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,4 +2,14 @@ const formatDecimals = (num) => {
   return Number.parseFloat(num).toFixed(2);
 };
 
-export { formatDecimals };
+const calulateCurrentPrice = (product) => {
+  if (product.isDiscount) {
+    const discountFactor = 1 - product.discountPercentage / 100;
+
+    return product.price * discountFactor;
+  }
+
+  return product.price;
+};
+
+export { formatDecimals, calulateCurrentPrice };
