a5e560ebc4dfb25ae3bedc5939fc1ea5cf442151
Esa <esa-pitkanen@hotmail.com>
2026-04-19 14:16:05 +0300

    feat: apply weekly discount section to the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,16 @@
 import Carousel from "../features/slideshow/Carousel.jsx";
 import FreshGroceriesSection from "../features/home/FreshGroceriesSection.jsx";
+import DiscountSection from  "../features/home/DiscountSection.jsx";
 import { useOutletContext } from "react-router";
 import "../styles/Home.css";
 
 const Home = () => {
-  const { data } = useOutletContext();
+  const { data, handleAddToCart } = useOutletContext();
 
   return (
     <>
       <Carousel data={data} />
+      <DiscountSection data={data} handleAddToCart={handleAddToCart} />
       <FreshGroceriesSection />
     </>
   );
