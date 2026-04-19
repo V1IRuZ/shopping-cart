d22fe32044bc2aabdc60dbaf18001ea05ef27933
Esa <esa-pitkanen@hotmail.com>
2026-04-19 17:40:17 +0300

    feat: apply DeliverySection to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Carousel from "../features/slideshow/Carousel.jsx";
 import FreshGroceriesSection from "../features/home/FreshGroceriesSection.jsx";
 import DiscountSection from  "../features/home/DiscountSection.jsx";
 import FurnitureSection from "../features/home/FurnitureSection.jsx";
+import DeliverySection from "../features/home/DeliverySection.jsx";
 import { useOutletContext } from "react-router";
 import "../styles/Home.css";
 
@@ -13,6 +14,7 @@ const Home = () => {
       <Carousel data={data} />
       <DiscountSection data={data} handleAddToCart={handleAddToCart} />
       <FreshGroceriesSection />
+      <DeliverySection />
       <FurnitureSection />
     </>
   );
