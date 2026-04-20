9766d9208b06adfa7e7a4bcec30af74d79e5fc3f
Esa <esa-pitkanen@hotmail.com>
2026-04-20 16:52:52 +0300

    refactor: extract price styles into its own module

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,15 +1,16 @@
 import { formatDecimals } from "../utils/helpers.js";
+import styles from "../styles/Price.module.css";
 
-const Price = ({ product, className }) => {
+const Price = ({ product }) => {
   if (product.isDiscount) {
     const discountPercentage = 1 - (product.discountPercentage / 100);
     const disCountedPrice = product.price * discountPercentage;
     const fixedDiscountedPrice = formatDecimals(disCountedPrice)
 
     return (
-      <h2 className={`${className} discount`}>
-        <span className="new">{fixedDiscountedPrice}$</span>
-        <span className="old">{product.price}$</span>
+      <h2 className={styles.price}>
+        <span >{fixedDiscountedPrice}$</span>
+        <span className={styles.regular}>{product.price}$</span>
       </h2>
     );
   }
--- src/features/shop/ProductCard.jsx
+++ src/features/shop/ProductCard.jsx
@@ -12,7 +12,7 @@ const ProductCard = ({ product, onAddtoCart }) => {
         <div className="product-info">
           <h1 className="product-title">{product.title}</h1>
           <StarRating key={product.id} rating={product.rating} />
-          <Price product={product} className="product-price" />
+          <Price product={product} />
         </div>
       </Link>
       <Button label="ADD TO CART" className="add-btn" onClick={onAddtoCart} />
