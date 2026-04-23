59c5237f19917fc3f7593f263142aa4945805ee0
Esa <esa-pitkanen@hotmail.com>
2026-04-23 19:02:01 +0300

    feat: add test ids for icons

--- src/features/product/AvailabilityStatus.jsx
+++ src/features/product/AvailabilityStatus.jsx
@@ -4,9 +4,9 @@ const AvailabilityStatus = ({ availability }) => {
   return (
     <div className="stock">
       {availability === "In Stock" ? (
-        <CircleCheck color="green" />
+        <CircleCheck data-testid="high" color="green" />
       ) : (
-        <CircleCheck color="orange" />
+        <CircleCheck data-testid="low" color="orange" />
       )}
       <span>{availability}</span>
     </div>
