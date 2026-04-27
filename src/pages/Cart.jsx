8ade15af4c3a90f195ff537b54737c4139670e96
Esa <esa-pitkanen@hotmail.com>
2026-04-27 12:39:15 +0300

    feat: apply EmptyCart component if cart is empty

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,18 @@
 import { useOutletContext } from "react-router";
 import CartList from "../features/cart/CartList.jsx";
 import Checkout from "../features/cart/Checkout.jsx";
+import EmptyCart from "../features/cart/EmptyCart.jsx";
 import "../styles/Cart.css";
 
 const Cart = () => {
   const { cart, setCart } = useOutletContext();
 
   const totalItems = cart.reduce((total, item) => total + item.quantity, 0);
+  const emptyCart = cart.length <= 0;
+
+  if (emptyCart) {
+    return <EmptyCart />
+  }
 
   return (
     <>
