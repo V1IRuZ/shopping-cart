8e82b44b1cd3945188f58d42cb85aa9904fa423b
Esa <esa-pitkanen@hotmail.com>
2026-05-01 16:05:17 +0300

    refactor: change notification to a link to the shopping cart

--- src/features/cart/Notification.jsx
+++ src/features/cart/Notification.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import { X } from "lucide-react";
 import styles from "../../styles/Notification.module.css";
 
@@ -7,12 +8,12 @@ const Notification = ({ cart, currentProductId }) => {
   if (!currentCartProduct) return null;
 
   return (
-    <div className={styles.notification}>
+    <Link to="/cart" className={styles.notification}>
       <img src={currentCartProduct.image} alt="" className={styles.image} />
       <span>{currentCartProduct.title}</span>
       <X />
       <span className={styles.quantity}>{currentCartProduct.quantity}</span>
-    </div>
+    </Link>
   );
 };
 
