238fb21be99c6bd88e9bbed1f0e2a512f9824717
Esa <esa-pitkanen@hotmail.com>
2026-05-03 16:04:25 +0300

    feat: add setup for opening and closing native dialog

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,8 +1,18 @@
-import { expect, afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
+import { expect, afterEach, beforeAll } from "vitest";
+import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
 
 expect.extend(matchers);
+
+beforeAll(() => {
+  HTMLDialogElement.prototype.showModal = function () {
+    this.open = true;
+  };
+
+  HTMLDialogElement.prototype.close = function () {
+    this.open = false;
+  };
+});
 
 afterEach(() => {
   cleanup();
