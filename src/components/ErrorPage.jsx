7ba4eddc3f2338604a8aa67b0a2272d035bac47e
Esa <esa-pitkanen@hotmail.com>
2026-05-03 17:45:11 +0300

    style: adjust text and icon size

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -3,8 +3,8 @@ import { CircleX } from "lucide-react";
 const ErrorPage = () => {
   return (
     <div className="error">
-      <CircleX size={36} color="red" />
-      <h1>Something went wrong, try again later.</h1>
+      <CircleX size={108} color="red" />
+      <h2>Something went wrong, try again later.</h2>
     </div>
   );
 };
