8171361d794202bf313c446e2b321457a9b78e21
Esa <esa-pitkanen@hotmail.com>
2026-05-07 13:23:56 +0300

    style(home): adjust groceries, furniture and delivery section responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Carousel from "../features/slideshow/Carousel.jsx";
 import FreshGroceriesSection from "../features/home/FreshGroceriesSection.jsx";
-import DiscountSection from  "../features/home/DiscountSection.jsx";
+import DiscountSection from "../features/home/DiscountSection.jsx";
 import FurnitureSection from "../features/home/FurnitureSection.jsx";
 import DeliverySection from "../features/home/DeliverySection.jsx";
 import { useOutletContext } from "react-router";
