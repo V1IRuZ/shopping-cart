f3b245d3828e7c38fe3d3354b9e46e8a369448ee
Esa <esa-pitkanen@hotmail.com>
2026-05-08 20:34:48 +0300

    test: add test cases for useCart hook

---
+++ src/hooks/__tests__/useCart.test.js
@@ -0,0 +1,106 @@
+import { renderHook, waitFor } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import { useCart } from "../useCart.js";
+
+const mockData = {
+  title: "chair",
+  id: 1,
+  category: "furniture",
+  discountPercentage: 10,
+  isDiscount: true,
+  image: undefined,
+  price: 10.0,
+};
+
+const mockData2 = {
+  title: "table",
+  id: 2,
+  category: "furniture",
+  discountPercentage: 10,
+  isDiscount: true,
+  image: undefined,
+  price: 10.0,
+};
+
+describe("useCart", () => {
+  it("initial state should be empty array", () => {
+    const { result } = renderHook(() => useCart());
+
+    expect(result.current.cart).toEqual([]);
+  });
+
+  it("should add product to the cart", async () => {
+    const { result } = renderHook(() => useCart());
+
+    result.current.handleAddToCart(mockData);
+
+    await waitFor(() => {
+      expect(result.current.cart).toEqual([
+        {
+          title: "chair",
+          id: 1,
+          category: "furniture",
+          discountPercentage: 10,
+          isDiscount: true,
+          image: undefined,
+          price: 10.0,
+          quantity: 1,
+        },
+      ]);
+    });
+  });
+
+  it("should add multiple products to the cart", async () => {
+    const { result } = renderHook(() => useCart());
+
+    result.current.handleAddToCart(mockData);
+    result.current.handleAddToCart(mockData2);
+
+    await waitFor(() => {
+      expect(result.current.cart).toEqual([
+        {
+          title: "chair",
+          id: 1,
+          category: "furniture",
+          discountPercentage: 10,
+          isDiscount: true,
+          image: undefined,
+          price: 10.0,
+          quantity: 1,
+        },
+        {
+          title: "table",
+          id: 2,
+          category: "furniture",
+          discountPercentage: 10,
+          isDiscount: true,
+          image: undefined,
+          price: 10.0,
+          quantity: 1,
+        },
+      ]);
+    });
+  });
+
+  it("should increase quanitity of the product in the cart", async () => {
+    const { result } = renderHook(() => useCart());
+
+    result.current.handleAddToCart(mockData);
+    result.current.handleAddToCart(mockData);
+
+    await waitFor(() => {
+      expect(result.current.cart).toEqual([
+        {
+          title: "chair",
+          id: 1,
+          category: "furniture",
+          discountPercentage: 10,
+          isDiscount: true,
+          image: undefined,
+          price: 10.0,
+          quantity: 2,
+        },
+      ]);
+    });
+  });
+});
